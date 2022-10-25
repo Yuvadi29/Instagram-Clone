bc64f43360b40f2cd258d0f4177447310f844683
Zaid1681 <95166179+Zaid1681@users.noreply.github.com>
2022-10-25 09:00:33 -0700

    main homepage of insta-clone

--- src/components/home/Homepage/Homepage.js
+++ src/components/home/Homepage/Homepage.js
@@ -1,1 +1,21 @@
+import React from 'react'
+import Maincontent from '../Maincontent/Maincontent'
+import Navbar from '../Navbar/Navbar'
+import "./Homepage.css"
 
+
+const homepage = () => {
+  return (
+    <div  className='home_page'>
+        <div >
+            <Navbar /> 
+        </div>
+        <div>
+            <Maincontent />
+        </div>
+      
+    </div>
+  )
+}
+
+export default homepage
