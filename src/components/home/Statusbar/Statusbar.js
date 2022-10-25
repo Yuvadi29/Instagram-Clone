7fd8c96f083a24d4febb4888010224bfa1bd7ce3
Zaid1681 <95166179+Zaid1681@users.noreply.github.com>
2022-10-25 09:05:20 -0700

    Add files via upload

---
+++ src/components/home/Statusbar/Statusbar.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import "./Statusbar.css"
+import Avatar from '@mui/material/Avatar';
+import pp1 from "../../images/pp1.png"
+
+
+
+
+const Statusbar = () => {
+    return (
+        <div className='status_bar'>
+            <div>
+                 <Avatar style={{ "width": "50px", "height": "50px", "display": "inline-block" ,"border" : "2px solid red"}} src={pp1} className="status_icnos" />
+                    <div>       <p className='status_text'>zaidkhan</p>
+                    </div> 
+
+            </div>
+            <div>
+                 <Avatar style={{ "width": "50px", "height": "50px", "display": "inline-block", "border" : "2px solid red"}} src={pp1} className="status_icnos" />
+                    <div>       <p className='status_text'>zaidkhan</p>
+                    </div> 
+
+            </div>
+            <div>
+                 <Avatar style={{ "width": "50px", "height": "50px", "display": "inline-block", "border" : "2px solid red"}} src={pp1} className="status_icnos" />
+                    <div>       <p className='status_text'>zaidkhan</p>
+                    </div> 
+
+            </div>
+            <div>
+                 <Avatar style={{ "width": "50px", "height": "50px", "display": "inline-block" ,"border" : "2px solid red"}} src={pp1} className="status_icnos" />
+                    <div>       <p className='status_text'>zaidkhan</p>
+                    </div> 
+
+            </div>
+            <div>
+                 <Avatar style={{ "width": "50px", "height": "50px", "display": "inline-block", "border" : "2px solid red"}} src={pp1} className="status_icnos" />
+                    <div>       <p className='status_text'>zaidkhan</p>
+                    </div> 
+
+            </div>
+            <div>
+                 <Avatar style={{ "width": "50px", "height": "50px", "display": "inline-block", "border" : "2px solid red"}} src={pp1} className="status_icnos" />
+                    <div>       <p className='status_text'>zaidkhan</p>
+                    </div> 
+
+            </div>
+            <div>
+                 <Avatar style={{ "width": "50px", "height": "50px", "display": "inline-block", "border" : "2px solid red"}} src={pp1} className="status_icnos" />
+                    <div>       <p className='status_text'>zaidkhan</p>
+                    </div> 
+
+            </div>
+            <div>
+                 <Avatar style={{ "width": "50px", "height": "50px", "display": "inline-block" ,"border" : "2px solid red"}} src={pp1} className="status_icnos" />
+                    <div>       <p className='status_text'>zaidkhan</p>
+                    </div> 
+
+            </div>
+            <div>
+                 <Avatar style={{ "width": "50px", "height": "50px", "display": "inline-block" ,"border" : "2px solid red"}} src={pp1} className="status_icnos" />
+                    <div>       <p className='status_text'>zaidkhan</p>
+                    </div> 
+
+            </div>
+           
+        </div>
+
+    )
+}
+
+export default Statusbar
