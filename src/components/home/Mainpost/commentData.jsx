7e1d4d4b42c75ebea63caeeff189c847adfcb0fa
Zaid1681 <95166179+Zaid1681@users.noreply.github.com>
2022-10-25 09:03:46 -0700

    Add files via upload

---
+++ src/components/home/Mainpost/commentData.jsx
@@ -0,0 +1,18 @@
+const commentData = [
+    {
+        key : "23",
+        username : "theashish" ,
+        comment : "It is very beutiful scenerio"
+    
+      },
+    {
+        key : "24",
+        username : "theayan" ,
+        comment : "It is very a great day"
+    
+      }
+      
+  ];
+  
+  export default commentData;
+  
