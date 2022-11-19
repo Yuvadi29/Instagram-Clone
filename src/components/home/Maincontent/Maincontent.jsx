c50333215ef17eda5f0fc133b3ec2b4fa84b83a0
[Aditya,Trivedi] <[adityatrivedi612@gmail.com]>
2022-11-19 11:19:15 +0530

    Stories bar with different user image

--- src/components/home/Maincontent/Maincontent.jsx
+++ src/components/home/Maincontent/Maincontent.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Grid from '@mui/material/Grid';
 import "./Maincontent.css";
 import Stories from "../Stories/Stories";
--- src/components/home/Stories/Stories.jsx
+++ src/components/home/Stories/Stories.jsx
@@ -1,55 +1,67 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import "./Stories.css";
 import Avatar from '@mui/material/Avatar';
-import pp1 from "../../images/pp1.png";
+import user1 from "../../images/user1.jpg";
+import user2 from "../../images/user2.jpg";
+import user3 from "../../images/user3.jpg";
+import user4 from "../../images/user4.jpg";
+import user5 from "../../images/user5.jpg";
+import user6 from "../../images/user6.jpg";
+import user7 from "../../images/user7.jpg";
+import user8 from "../../images/user8.jpg";
+import user9 from "../../images/user9.jpg";
+import user10 from "../../images/user10.jpg";
+import user11 from "../../images/user11.jpg";
+import user12 from "../../images/user12.jpg";
 
 const data = [
         {
-                image: pp1,
+                image: user1,
                 profile_name: 'Zaid'
         },
         {
-                image: pp1,
+                image: user2,
                 profile_name: 'Jay '
         },
         {
-                image: pp1,
+                image: user3,
                 profile_name: 'Aditya'
         },
         {
-                image: pp1,
+                image: user4,
                 profile_name: 'Vinish'
         },
         {
-                image: pp1,
+                image: user5,
                 profile_name: 'Ashish'
         },
         {
-                image: pp1,
+                image: user6,
                 profile_name: 'Bhuvan'
         },
         {
-                image: pp1,
+                image: user7,
                 profile_name: 'Anish'
         },
         {
-                image: pp1,
+                image: user8,
                 profile_name: 'Ajey'
         },
         {
-                image: pp1,
+                image: user9,
                 profile_name: 'Kedar'
         },
         {
-                image: pp1,
+                image: user10,
                 profile_name: 'Aman'
         },
         {
-                image: pp1,
+                image: user11,
                 profile_name: 'Harsh'
         },
         {
-                image: pp1,
+                image: user12,
                 profile_name: 'Abhay'
         },
 ]
@@ -61,15 +73,18 @@ const Stories = () => {
                                 data.map(({ image, profile_name }) => {
                                         return (
                                                 <div>
-                                                        <Avatar src={image} alt="profile_image" className="stories_icon" />
-                                                        <p className="profile_name">{profile_name}</p>
+                                                        <Link to="profile">
+                                                                <Avatar src={image}
+                                                                        className='stories_icon'
+                                                                        style={{ width: '50px', height: '50px', margin: '5px' }}
+                                                                />
+                                                                <p className="profile_name">{profile_name}</p>
+                                                        </Link>
                                                 </div>
                                         )
                                 })
                         }
                 </div>
-
         )
 }
-
 export default Stories
